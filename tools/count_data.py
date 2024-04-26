6888702c33063967d94f26283eac5bdd57eb1567
Xinlu Lai <35400185+CrazyBoyM@users.noreply.github.com>
2024-04-27 00:33:41 +0800

    添加数据统计分析脚本

---
+++ tools/count_data.py
@@ -0,0 +1,23 @@
+def count_jsonl(input_file):
+    max_len = 0
+    min_len = 1e9
+    total_len = 0
+    count = 0
+    with open(input_file, 'r', encoding='utf-8') as f_input:
+        for line in f_input:
+            if len(line) > max_len:
+                max_len = len(line)
+            if len(line) < min_len:
+                min_len = len(line)
+            total_len += len(line)
+            count += 1
+        
+        print("max_len: ", max_len)
+        print("min_len: ", min_len)
+        print("total_len: ", total_len)
+        print(f"count: {count}")
+        print(f"average_len: {total_len / count}")
+
+if __name__ == "__main__":
+    input_file = "./LongQLoRA-SFT_13k.jsonl"
+    count_jsonl(input_file)
