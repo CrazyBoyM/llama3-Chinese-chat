f8cdc880d6e76347833b7fdf5e03d9f64fb1352f
Xinlu Lai <35400185+CrazyBoyM@users.noreply.github.com>
2024-05-02 02:09:04 +0800

    Update change_info.py

--- tools/change_info.py
+++ tools/change_info.py
@@ -1,12 +1,18 @@
 def replace_keywords_and_remove_lines(input_file, output_file):
     # 定义关键词替换规则
     keywords = {
-        "ChatGPT": "LLama3-Chinese",
-        "GPT3.5": "LLama3-Chinese",
-        "GPT-3": "LLama3-Chinese",
+        "ChatGPT": "shareAI-GPT",
+        "GPT3.5": "shareAI-GPT",
+        "Gpt3.5": "shareAI-GPT",
+        "gpt3.5": "shareAI-GPT",
+        "GPT-3": "shareAI-GPT",
+        "gpt-3": "shareAI-GPT",
+        "OpenAI": "shareAI",
+        "openAI": "shareAI",
+        "openai": "shareAI",
     }
     # 定义要删除的关键词
-    delete_keywords = ["无法", "OpenAI", "抱歉", "Sorry", "对不起", "道德"]
+    delete_keywords = ["无法", "不能", "can't", "can not"]
 
     with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
         for line in input_f:
